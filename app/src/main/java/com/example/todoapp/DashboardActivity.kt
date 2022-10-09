e3ce0a55b7248a1ff5c4cdae2d5f5906b01271ae
Laxman <laxi27902@gmail.com>
2022-10-09 10:20:54 +0530

    Very much done with UI

--- app/src/main/java/com/example/todoapp/DashboardActivity.kt
+++ app/src/main/java/com/example/todoapp/DashboardActivity.kt
@@ -1,7 +1,16 @@
 package com.example.todoapp
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.recycleradapter.Adapter
@@ -11,13 +20,40 @@ class DashboardActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var myAdapter: Adapter
+    private lateinit var btnAdd: Button
 
     private var rvTaskArray = ArrayList<EachTask>()
+    private lateinit var dailogBinding: View
+    private lateinit var myDial: Dialog
+    private lateinit var updateDailogBinding: View
+    private lateinit var updateDial: Dialog
+
+    private lateinit var ivDeleteAll : ImageView
+    private lateinit var btnSave : Button
+    private lateinit var createdTitle : EditText
+    private lateinit var createdPriority : EditText
+    private lateinit var tvAlertPriority : TextView
+    private lateinit var ivEmpty : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+
+        dailogBinding = layoutInflater.inflate(R.layout.create_card, null)
+        myDial = Dialog(this)
+
+        updateDailogBinding = layoutInflater.inflate(R.layout.update_card, null)
+        updateDial = Dialog(this)
+
+        btnSave = dailogBinding.findViewById(R.id.btnSave)
+        createdTitle = dailogBinding.findViewById(R.id.create_title)
+        createdPriority = dailogBinding.findViewById(R.id.create_priority)
+        tvAlertPriority = dailogBinding.findViewById(R.id.tvAlertPriority)
+
+        ivEmpty = findViewById(R.id.ivEmpty)
+        myDial.setContentView(dailogBinding)
+        updateDial.setContentView(updateDailogBinding)
 //        Recycler view Initialization starts
         recyclerView = findViewById(R.id.recycler_view)
         recyclerView.setHasFixedSize(true)
@@ -25,38 +61,124 @@ class DashboardActivity : AppCompatActivity() {
 //        Recycler view Initialization ends
 
 //        list of Task Initialization starts
+        ivDeleteAll = findViewById(R.id.ivDeleteAll)
+        btnAdd = findViewById(R.id.add)
         rvTaskArray = ArrayList()
         myAdapter = Adapter(rvTaskArray)
 //        list of Task Initialization ends
 
 
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","low"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","low"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","low"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","low"))
-        rvTaskArray.add(EachTask("Bath","low"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","high"))
-        rvTaskArray.add(EachTask("Bath","medium"))
-        rvTaskArray.add(EachTask("Bath","high"))
-
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "low"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "low"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "low"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "low"))
+//        rvTaskArray.add(EachTask("Bath", "low"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+//        rvTaskArray.add(EachTask("Bath", "medium"))
+//        rvTaskArray.add(EachTask("Bath", "high"))
+emptyImageShow(rvTaskArray)
         recyclerView.adapter = myAdapter
 
+        ivDeleteAll.setOnClickListener{
+            rvTaskArray.clear()
+            recyclerView.adapter = myAdapter
+           emptyImageShow(rvTaskArray)
+        }
+
+        btnAdd.setOnClickListener {
+
+//            Show Dialog
+            myDial.setCancelable(true)
+            myDial.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDial.show()
+
+//              Save Task
+                btnSave.setOnClickListener {
+                      val finalTitle = createdTitle.getText().toString()
+                    val finalprio = createdPriority.getText().toString().toLowerCase()
+
+                    Log.i("TAG","PRIO is $finalprio")
+
+                    if (finalTitle.isNullOrEmpty()){
+
+                        Toast.makeText(this,"Please Enter Title",Toast.LENGTH_SHORT).show()
+
+                    }  else if (finalprio.isNullOrEmpty()){
+
+                        Toast.makeText(this,"Please Set Priority of your task",Toast.LENGTH_SHORT).show()
+
+                    }  else if (finalprio.isNotBlank() && finalTitle.isNotBlank()){
+
+                      if ((finalprio == "high" || finalprio == "low"||  finalprio == "medium")) {
+                            rvTaskArray.add(EachTask(finalTitle,finalprio))
+                            emptyImageShow(rvTaskArray)
+                            recyclerView.adapter = myAdapter
+                            myDial.dismiss()
+
+                            createdTitle.setText("")
+                            createdPriority.setText("")
+                            tvAlertPriority.visibility = View.GONE
+
+                        } else {
+                         tvAlertPriority.visibility = View.VISIBLE
+                        }
+
+                    }
+
+                }
+
+            //              Save Task
+
+            myAdapter.onClick = {
+                updateDial.setCancelable(true)
+                updateDial.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                updateDial.show()
+            }
+
+
+        }
+
+    }
+
+    private fun emptyImageShow(rvTaskArray: ArrayList<EachTask>) {
+
+        if (rvTaskArray.isNotEmpty()){
+            ivEmpty.visibility = View.GONE
+        } else { ivEmpty.visibility = View.VISIBLE}
+    }
+
+
+    private var doubleBackToExitPressedOnce = false
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+
+        this.doubleBackToExitPressedOnce = true
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            doubleBackToExitPressedOnce = false
+        }, 2000)
     }
 }
+
+
--- app/src/main/java/com/example/todoapp/recycleradapter/Adapter.kt
+++ app/src/main/java/com/example/todoapp/recycleradapter/Adapter.kt
@@ -14,6 +14,9 @@ import com.example.todoapp.R
 
 class Adapter(private val task:List<EachTask>) : RecyclerView.Adapter<Adapter.MyViewHolder>() {
 
+    var onClick : ((EachTask) -> Unit)? = null
+
+
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         var title : TextView = itemView.findViewById(R.id.title)
         var priority : TextView = itemView.findViewById(R.id.priority)
@@ -27,8 +30,6 @@ class Adapter(private val task:List<EachTask>) : RecyclerView.Adapter<Adapter.My
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
-
-
         val item = task[position]
         holder.title.text = item.taskTitle
         holder.priority.text = item.taskPriority
@@ -41,10 +42,20 @@ class Adapter(private val task:List<EachTask>) : RecyclerView.Adapter<Adapter.My
             "medium" -> holder.cardBg.setCardBackgroundColor(ContextCompat.getColor(holder.cardBg.context, R.color.medium))
 
         }
+
+        holder.itemView.setOnClickListener {
+            onClick?.invoke(item)
+        }
+
     }
 
     override fun getItemCount(): Int {
        return task.size
     }
 
+}
+
+
+interface recyclerItem{
+    fun onItemClicked(view: View)
 }
